801009640dddbb4cda7bf164797e45acad609ea5
AlexandrWalker <alexandr.d.walker@gmail.com>
2025-03-03 19:51:05 +0700

    Updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,56 @@
 (() => {
   document.addEventListener('DOMContentLoaded', () => {
 
+    var hero__slider = new Swiper(".hero__slider-init", {
+      spaceBetween: 30,
+      slidesPerView: 1,
+      centeredSlides: true,
+      loop: true,
+      speed: 600,
+      mousewheel: {
+        forceToAxis: true,
+      },
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: false
+      },
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
 
-
+    var feat__slider = new Swiper(".feat__slider-init", {
+      slidesPerView: "auto",
+      spaceBetween: 10,
+      speed: 600,
+      mousewheel: {
+        forceToAxis: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      grid: false,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      breakpoints: {
+        601: {
+          spaceBetween: 20,
+          slidesPerView: 2,
+          grid: {
+            rows: 2,
+          },
+        }
+      },
+    });
   });
 })();
